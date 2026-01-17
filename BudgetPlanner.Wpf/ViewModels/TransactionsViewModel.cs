28cc4807752c65fffc25eef82385ceca52f3597b
David Browning <david.a.browning@gmail.com>
2026-01-17 11:51:20 +0100

    Add TransactionsViewModel.

---
+++ BudgetPlanner.Wpf/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using BudgetPlanner.Core.Dtos;
+using BudgetPlanner.Core.Enums;
+using System.Collections.ObjectModel;
+
+namespace BudgetPlanner.Wpf.ViewModels
+{
+    public class TransactionsViewModel
+    {
+        private ObservableCollection<TransactionDto> transactions = new();
+        public ObservableCollection<TransactionDto> Transactions
+        {
+            get { return transactions; }
+            set { transactions = value; }
+        }
+
+        public TransactionsViewModel()
+        {
+            transactions.Add(new TransactionDto() { Amount = 30000, Category = "Lön", Frequency = Frequency.Monthly, Comment = "Monthly salary" });
+            transactions.Add(new TransactionDto() { Amount = -5000, Category = "Hyra", Frequency = Frequency.Monthly, Comment = "Monthly rent" });
+        }
+
+        public async Task LoadAsync()
+        {
+            if (transactions.Any())
+            {
+                return;
+            }
+
+            // Otherwise, load transactions from database
+        }
+    }
+}
