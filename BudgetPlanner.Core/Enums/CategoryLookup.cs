678e308cfb996002d0bc50a822915c18e97e3cea
David Browning <david.a.browning@gmail.com>
2026-01-18 23:01:54 +0100

    Use default frequency value.

---
+++ BudgetPlanner.Core/Enums/CategoryLookup.cs
@@ -0,0 +1,21 @@
+﻿namespace BudgetPlanner.Core.Enums
+{
+    public static class CategoryLookup
+    {
+        public static List<Category> All = new()
+        {
+            (Category)0,
+            (Category)1, 
+            (Category)2,
+            (Category)3,
+            (Category)4, 
+            (Category)5,
+            (Category)6,
+            (Category)7,
+            (Category)8,
+            (Category)9,
+            (Category)10,
+            (Category)11,
+        };
+    }
+}
---
+++ BudgetPlanner.Core/Enums/FrequencyLookup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Core.Enums
+{
+    public static class FrequencyLookup
+    {
+        public static List<Frequency> All = new() { 
+            Frequency.OneTime, 
+            Frequency.Monthly, 
+            Frequency.Yearly 
+        };
+    }
+}
