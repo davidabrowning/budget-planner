4caea44c256a2e86ff38b28bc3fab8290eff8e9a
David Browning <david.a.browning@gmail.com>
2026-01-30 13:48:28 +0100

    Preselect frequency.

--- BudgetPlanner.Wpf/ViewModels/TransactionsViewModel.cs
+++ BudgetPlanner.Wpf/ViewModels/TransactionsViewModel.cs
@@ -35,6 +35,7 @@ namespace BudgetPlanner.Wpf.ViewModels
         public IEnumerable<Category> AllCategories { get { return CategoryLookup.All; } }
         public Category DefaultCategory { get { return Category.Unknown; } }
         public IEnumerable<Frequency> AllFrequencies { get { return FrequencyLookup.All; } }
+        public Frequency DefaultFrequency { get { return Frequency.OneTime; } }
         public IEnumerable<Month> AllMonths { get { return MonthLookup.All; } }
 
         public TransactionsViewModel(ITransactionService transactionService)
