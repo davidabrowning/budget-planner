912b951042683f53a040df9a124873742b1fef09
David Browning <david.a.browning@gmail.com>
2026-01-30 23:36:29 +0100

    Move MainWindow into Views directory.

--- BudgetPlanner.Wpf/Views/MainWindow.xaml.cs
+++ BudgetPlanner.Wpf/Views/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BudgetPlanner.Wpf
+namespace BudgetPlanner.Wpf.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
