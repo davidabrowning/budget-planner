42a3434d82e815be7860e06fe054f3221665fb6f
David Browning <david.a.browning@gmail.com>
2026-02-01 13:38:44 +0100

    Add DbContext.

---
+++ BudgetPlanner.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BudgetPlanner.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
